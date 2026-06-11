63ad5992ec253e3d751266e956a9ba3d3469d0a2
Stone Tao <stonezt2019@gmail.com>
2026-06-10 21:13:11 -0700

    cleanup

--- mani_skill/envs/tasks/__init__.py
+++ mani_skill/envs/tasks/__init__.py
@@ -1,11 +1,47 @@
-from .control import *
-from .dexterity import *
-from .digital_twins import *
-from .drawing import *
-from .empty_env import EmptyEnv
-from .fmb import *
-from .humanoid import *
-from .mobile_manipulation import *
-from .quadruped import *
-from .rotate_cube import RotateCubeEnv
-from .tabletop import *
+from .control import (
+    CartpoleBalanceEnv as CartpoleBalanceEnv,
+    CartpoleSwingUpEnv as CartpoleSwingUpEnv,
+    HopperHopEnv as HopperHopEnv,
+    HopperStandEnv as HopperStandEnv,
+    HumanoidRun as HumanoidRun,
+    HumanoidStand as HumanoidStand,
+    HumanoidWalk as HumanoidWalk,
+    AntWalk as AntWalk,
+    AntRun as AntRun,
+)
+from .dexterity import InsertFlowerEnv as InsertFlowerEnv
+from .digital_twins import SO100GraspCubeEnv as SO100GraspCubeEnv
+from .drawing import (
+    DrawTriangleEnv as DrawTriangleEnv,
+    DrawSVGEnv as DrawSVGEnv,
+    TableTopFreeDrawEnv as TableTopFreeDrawEnv,
+)
+from .empty_env import EmptyEnv as EmptyEnv
+from .humanoid import (
+    HumanoidPlaceAppleInBowl as HumanoidPlaceAppleInBowl,
+    TransportBoxEnv as TransportBoxEnv,
+)
+from .mobile_manipulation import (
+    RoboCasaKitchenEnv as RoboCasaKitchenEnv,
+    OpenCabinetDoorEnv as OpenCabinetDoorEnv,
+    OpenCabinetDrawerEnv as OpenCabinetDrawerEnv,
+)
+from .tabletop import (
+    AssemblingKitsEnv as AssemblingKitsEnv,
+    LiftPegUprightEnv as LiftPegUprightEnv,
+    PegInsertionSideEnv as PegInsertionSideEnv,
+    PickClutterYCBEnv as PickClutterYCBEnv,
+    PickCubeEnv as PickCubeEnv,
+    PickSingleYCBEnv as PickSingleYCBEnv,
+    PlugChargerEnv as PlugChargerEnv,
+    PullCubeEnv as PullCubeEnv,
+    PushCubeEnv as PushCubeEnv,
+    StackCubeEnv as StackCubeEnv,
+    TurnFaucetEnv as TurnFaucetEnv,
+    PokeCubeEnv as PokeCubeEnv,
+    PlaceSphereEnv as PlaceSphereEnv,
+    RollBallEnv as RollBallEnv,
+    PushTEnv as PushTEnv,
+    PullCubeToolEnv as PullCubeToolEnv,
+    StackPyramidEnv as StackPyramidEnv,
+)
